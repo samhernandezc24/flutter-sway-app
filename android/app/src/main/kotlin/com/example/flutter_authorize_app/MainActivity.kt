2be549c299af8c3d0bd1489f753231c772ad7436
samhernandezc24 <samd.hernandezc24@gmail.com>
2023-12-21 17:26:55 -0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_authorize_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_authorize_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AutorizaApp',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Este es un mensaje de prueba.'),
+        ],
+      ),
+    );
+  }
+}
+
